29eee7a9e5c5f0094beea48ca0d02275d3df5bdc
fwatanab <fwatanab@student.42tokyo.jp>
2024-01-27 12:04:23 +0900

    引数を大文字にして出力する関数

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int	ft_isupper(int c){
+	if ('a' <= c && c <= 'z')
+		return 1;
+	return 0;
+}
+
+void	print_upper(char *argv){
+	for(int i = 0; argv[i]; i++){
+		if (ft_isupper((int)argv[i]) == 1)
+			argv[i] = argv[i] - 32;
+	}
+	std::cout << argv;
+}
+
+int	main(int argc, char **argv){
+	if (argc == 1){
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+	int i = 1;
+	while (argv[i])
+		print_upper(argv[i++]);
+	std::cout << std::endl;
+	return 0;
+}
