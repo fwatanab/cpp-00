43fab20713e806a0e10f30e9db1f027517a85b57
Futa Watanabe <fwatanab@c1r14s1.42tokyo.jp>
2024-01-31 21:34:32 +0900

    電話帳に登録するname numberを追加

--- ex01/Contact.cpp
+++ ex01/Contact.cpp
@@ -2,8 +2,21 @@
 
 void	Contact::set_first_name(const std::string name){
 	f_name = name;
+	std::cout << f_name << std::endl;
 }
 
 void	Contact::set_last_name(const std::string name){
 	l_name = name;
+}
+
+void	Contact::set_nick_name(const std::string name){
+	nick_name = name;
+}
+
+void	Contact::set_phone_number(const std::string name){
+	phone_nbr = name;
+}
+
+void	Contact::set_darkest_secret(const std::string name){
+	darkest_secret = name;
 }
--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -5,12 +5,15 @@ class Contact{
 	private:
 		std::string	f_name;
 		std::string	l_name;
-		std::string	nickname;
+		std::string	nick_name;
 		std::string	phone_nbr;
 		std::string	darkest_secret;
 	public:
 		void	set_first_name(std::string name);
 		void	set_last_name(std::string name);
+		void	set_nick_name(std::string name);
+		void	set_phone_number(std::string name);
+		void	set_darkest_secret(std::string name);
 };
 
 #endif
