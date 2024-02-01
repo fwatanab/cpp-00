6dc8f74c4a150e84e466d6cd7cf3aa33cc617186
Futa Watanabe <fwatanab@c1r15s9.42tokyo.jp>
2024-02-01 19:39:47 +0900

    コードを見やすく修正

--- ex01/search_prompt.cpp
+++ ex01/search_prompt.cpp
@@ -6,7 +6,7 @@
 /*   By: fwatanab <fwatanab@student.42.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 19:27:43 by fwatanab          #+#    #+#             */
-/*   Updated: 2024/02/01 19:27:57 by fwatanab         ###   ########.fr       */
+/*   Updated: 2024/02/01 19:39:12 by fwatanab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ void	prompt_name(int index, std::string f_name, std::string l_name, std::string 
 	if (f_name.empty() && l_name.empty() && n_name.empty() && nbr.empty())
 		return ;
 	std::cout << "|----------|----------|----------|----------|----------|" << std::endl;
-	std::cout << "|" << std::right << std::setw(10) << index \
-		<< "|" << std::right << std::setw(10) << f_name \
-		<< "|" << std::right << std::setw(10) << l_name \
-		<< "|" << std::right << std::setw(10) << n_name \
-		<< "|" << std::right << std::setw(10) << nbr << "|" << std::endl;
+	std::cout << "|" << std::right << std::setw(10) << index;
+	std::cout << "|" << std::right << std::setw(10) << f_name;
+	std::cout << "|" << std::right << std::setw(10) << l_name;
+	std::cout << "|" << std::right << std::setw(10) << n_name;
+	std::cout << "|" << std::right << std::setw(10) << nbr << "|" << std::endl;
 }
