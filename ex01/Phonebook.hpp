fd12290f0f5d7f0f4f2ac5f12eb84bf2c2098f21
Futa Watanabe <fwatanab@c1r15s9.42tokyo.jp>
2024-02-01 18:32:57 +0900

    del check_command

--- ex01/Phonebook.hpp
+++ ex01/Phonebook.hpp
@@ -14,10 +14,12 @@ class Phonebook{
 		Phonebook();
 		void	add_contact();
 		void	search_contact();
+		void	search_prompt();
 };
 
 std::string	start_prompt();
 std::string	re_start_prompt();
 std::string	add_prompt(std::string name);
+void		prompt_name(int index, std::string f_name, std::string l_name, std::string n_name, std::string nbr);
 
 #endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,14 +1,5 @@
 #include "Phonebook.hpp"
 
-void	check_command(std::string cmd, Phonebook book){
-	if (cmd == "ADD")
-		book.add_contact();
-	else if (cmd == "SEARCH")
-		book.search_contact();
-	else if (cmd == "EXIT")
-		return ;
-}
-
 int	main(){
 	Phonebook	book;
 	std::string	cmd;
@@ -16,9 +7,12 @@ int	main(){
 	while (cmd != "EXIT")
 	{
 		cmd = start_prompt();
-		if (!(cmd == "ADD" || cmd == "SEARCH" || cmd == "EXIT") || cmd.empty())
+		while (!(cmd == "ADD" || cmd == "SEARCH" || cmd == "EXIT") || cmd.empty())
 			cmd = re_start_prompt();
-		check_command(cmd, book);
+		if (cmd == "ADD")
+			book.add_contact();
+		else if (cmd == "SEARCH")
+			book.search_contact();
 		if (std::cin.eof()){
 			std::cout << std::endl;
 			break ;
