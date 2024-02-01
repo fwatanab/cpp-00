ecf0eb8c54345379aaa1fc0864515b168892c4a3
Futa Watanabe <fwatanab@c1r15s9.42tokyo.jp>
2024-02-01 16:17:08 +0900

    Code Unification

--- ex01/Phonebook.cpp
+++ ex01/Phonebook.cpp
@@ -14,44 +14,42 @@ void	Phonebook::add_contact(){
 	std::string	cmd;
 
 	this->index = 0;
-	while (!std::cin.eof())
-	{
+	while (!std::cin.eof()){
 		cmd = add_prompt("first name");
 		if (!cmd.empty())
 			break ;
 	}
 	this->contacts[this->index].set_first_name(cmd);
 
-	while (!std::cin.eof())
-	{
+	while (!std::cin.eof()){
 		cmd = add_prompt("last name");
 		if (!cmd.empty())
 			break ;
 	}
 	this->contacts[this->index].set_last_name(cmd);
 
-	while (!std::cin.eof())
-	{
+	while (!std::cin.eof()){
 		cmd = add_prompt("nick name");
 		if (!cmd.empty())
 			break ;
 	}
 	this->contacts[this->index].set_nick_name(cmd);
 
-	while (!std::cin.eof())
-	{
+	while (!std::cin.eof()){
 		cmd = add_prompt("phone number");
 		if (!cmd.empty())
 			break ;
 	}
 	this->contacts[this->index].set_phone_number(cmd);
 
-	while (!std::cin.eof())
-	{
+	while (!std::cin.eof()){
 		cmd = add_prompt("darkest secret");
 		if (!cmd.empty())
 			break ;
 	}
 	this->contacts[this->index].set_darkest_secret(cmd);
-	this->index++;
+	if (this->index + 1 != 9)
+		this->index++;
+	else if (this->index + 1 == 9)
+		this->index = 0;
 }
