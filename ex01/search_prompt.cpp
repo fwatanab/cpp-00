25726345f627550dbf1cb917c67545ae1fe4dfe3
Futa Watanabe <fwatanab@c1r15s9.42tokyo.jp>
2024-02-01 19:32:36 +0900

    10文字区切りの右詰めになるように修正

--- ex01/search_prompt.cpp
+++ ex01/search_prompt.cpp
@@ -1,14 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   search_prompt.cpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fwatanab <fwatanab@student.42.jp>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/01 19:27:43 by fwatanab          #+#    #+#             */
+/*   Updated: 2024/02/01 19:27:57 by fwatanab         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Phonebook.hpp"
 
-void	Phonebook::search_prompt(){
+void	search_start_prompt(){
 	std::cout << "--------------------------------------------------------" << std::endl;
 	std::cout << "|     index|first name| last name| nick name| phone nbr|" << std::endl;
-	std::cout << "|----------|----------|----------|----------|----------|" << std::endl;
 }
 
 void	prompt_name(int index, std::string f_name, std::string l_name, std::string n_name, std::string nbr){
 	if (f_name.empty() && l_name.empty() && n_name.empty() && nbr.empty())
 		return ;
-	std::cout << "|         " << index << "|" << f_name << l_name << n_name << nbr << std::endl;
 	std::cout << "|----------|----------|----------|----------|----------|" << std::endl;
+	std::cout << "|" << std::right << std::setw(10) << index \
+		<< "|" << std::right << std::setw(10) << f_name \
+		<< "|" << std::right << std::setw(10) << l_name \
+		<< "|" << std::right << std::setw(10) << n_name \
+		<< "|" << std::right << std::setw(10) << nbr << "|" << std::endl;
 }
