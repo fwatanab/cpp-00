d38e75106f4828358397fc2f882d4f1ad9ca82b2
Futa Watanabe <fwatanab@c1r22s1.42tokyo.jp>
2024-02-03 16:25:50 +0900

    space -> \t

--- ex01/prompt.cpp
+++ ex01/prompt.cpp
@@ -4,9 +4,9 @@ std::string	start_prompt(){
 	std::string	cmd;
 
 	std::cout << "Please enter one of the following commands:\n" << std::endl;
-	std::cout << "ADD: Add a new contact.\n";
-	std::cout << "SEARCH: Search for an existing contact.\n";
-	std::cout << "EXIT: Exit the program.\n" << std::endl;
+	std::cout << "ADD:\tAdd a new contact.\n";
+	std::cout << "SEARCH:\tSearch for an existing contact.\n";
+	std::cout << "EXIT:\tExit the program.\n" << std::endl;
 	std::cout << "Enter your command: ";
 	std::getline(std::cin, cmd);
 	if (cmd != "EXIT")
@@ -18,9 +18,9 @@ std::string	re_start_prompt(){
 	std::string	cmd;
 
 	std::cout << "Invalid command. Please choose one of the following options:" << std::endl;
-	std::cout << "ADD: Add a new contact.\n";
-	std::cout << "SEARCH: Search for an existing contact.\n";
-	std::cout << "EXIT: Exit the program.\n" << std::endl;
+	std::cout << "ADD:\tAdd a new contact.\n";
+	std::cout << "SEARCH:\tSearch for an existing contact.\n";
+	std::cout << "EXIT:\tExit the program.\n" << std::endl;
 	std::cout << "Please enter a valid command: ";
 	std::getline(std::cin, cmd);
 	if (cmd != "EXIT")
