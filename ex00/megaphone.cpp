2007ba7842421d21d28467b048864d8468065f91
fwatanab <fwatanab@student.42tokyo.jp>
2024-04-17 14:48:10 +0900

    fix / C++ manner

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,27 +1,17 @@
 #include <iostream>
-
-int	ft_isupper(int c){
-	if ('a' <= c && c <= 'z')
-		return 1;
-	return 0;
-}
-
-void	print_upper(char *str) {
-	for(int i = 0; str[i]; i++){
-		if (std::islower(str[i]))
-			str[i] = std::toupper(str[i]);
-	}
-	std::cout << str;
-}
+#include <string>
 
 int	main(int argc, char **argv) {
 	if (argc == 1){
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return 0;
 	}
-	int i = 1;
-	while (argv[i])
-		print_upper(argv[i++]);
+	for (size_t j = 1; argv[j]; j++) {
+		std::string	str = argv[j];
+		for (size_t i = 0; i < str.size(); i++)
+			str[i] = std::toupper(str[i]);
+		std::cout << str;
+	}
 	std::cout << std::endl;
 	return 0;
 }
